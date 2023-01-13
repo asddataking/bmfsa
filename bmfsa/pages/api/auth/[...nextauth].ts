054e5a19e2003a4e65417b1bd9c53a74cfb5e7d1
Dan Richmond <40435281+asddataking@users.noreply.github.com>
2023-01-13 16:24:13 -0500

    Prisma/Supabase Connection Live
    Finally pushed Prisma Schema to Supabase

--- bmfsa/pages/api/auth/[...nextauth].ts
+++ bmfsa/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,17 @@
 import NextAuth from "next-auth"
 import { SupabaseAdapter } from "@next-auth/supabase-adapter"
+import GoogleProvider from "next-auth/providers/google"
 
 
 export default NextAuth({
   
-  providers: [...],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    
+  ],
   adapter: SupabaseAdapter({
     url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
